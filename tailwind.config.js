26a3710dbae6a48fb1f11f593d5dd514f7d7cc9d
Maishanu Ahidjo <maishanu.ahijo@yahoo.com>
2023-01-03 04:33:52 +0100

    screens added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}", 
-        "./components/**/*.{js,ts,jsx,tsx}"
-    ],
+    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     theme: {
         colors: {
             Secondary: "#FFFEFA",
             Primary: "#E3CAA5",
             Headers: "#3E3B32",
             Text: "#76715F",
+        },
+        screens: {
+            mb: "475px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
         },
     },
     plugins: [],

13a59d20ae892f3ce31c449bc7a84d61691d7f90
Maishanu Ahidjo <maishanu.ahijo@yahoo.com>
2023-01-04 02:27:31 +0100

    Product page added

---
+++ src/components/product-card/productCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Button from "../button/Button";
+import styles from "./productCard.module.scss";
+
+const ProductCard = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.image}></div>
+            <div className={styles.details}>
+                <div>
+                    <h4 className={styles.name}>Name name namw</h4>
+                    <h4 className={styles.price}># 1000</h4>
+                    <div className={styles.quantityCont}>
+                        <p className={styles.down}>-</p>
+                        <p className={styles.value}>1</p>
+                        <p className={styles.up}>+</p>
+                    </div>
+                </div>
+                <div>
+                    <p className={styles.description}>
+                        Lorem ipsum dolor sit amet elit consectetur adipisicing. Nemo, voluptatum!
+                    </p>
+                </div>
+            </div>
+            <Button text="Add to cart" width="full" />
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/pages/[product].jsx
+++ src/pages/[product].jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import ProductCard from "../components/product-card/productCard";
+import styles from "../styles/Product.module.scss";
 
 const Product = () => {
-    return <div></div>;
+    return (
+        <main className={styles.container}>
+            <header>
+                <h2>Lorem ipsum dolor sit.</h2>
+            </header>
+            <div className={styles.wrapper}>
+                <ProductCard />
+                <div className={styles.text}>
+                    <div className={styles.about}>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi repellat similique quasi
+                            illo ullam, iusto voluptatibus ea esse? Adipisci dolore quis suscipit perferendis harum
+                            deserunt ab facere temporibus corrupti, eligendi maiores. Exercitationem eius error aliquam?
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, nemo!
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nemo, mollitia quisquam
+                            illum temporibus cupiditate aperiam sed quos, maxime sit aut hic quae itaque eveniet. At
+                            impedit quaerat autem mollitia? Lorem ipsum dolor sit amet.
+                        </p>
+                    </div>
+                    <div className={styles.ingredents}>
+                        <h3>Ingredents</h3>
+                        <div className={styles.wrapper}>
+                            <div className={styles.ingredent}>
+                                <h5 className={styles.name}>Name Name</h5>
+                                <p className={styles.description}>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                </p>
+                                <p>100% | 0.8g</p>
+                            </div>
+                            <div className={styles.ingredent}>
+                                <h5 className={styles.name}>Name Name</h5>
+                                <p className={styles.description}>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                </p>
+                                <p>100% | 0.8g</p>
+                            </div>
+                            <div className={styles.ingredent}>
+                                <h5 className={styles.name}>Name Name</h5>
+                                <p className={styles.description}>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                </p>
+                                <p>100% | 0.8g</p>
+                            </div>
+                            <div className={styles.ingredent}>
+                                <h5 className={styles.name}>Name Name</h5>
+                                <p className={styles.description}>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                </p>
+                                <p>100% | 0.8g</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.images}>
+                <div className={styles.wrapper}>
+                    <div className={styles.img}></div>
+                    <div className={styles.img}></div>
+                </div>
+                <div className={styles.wrapper}>
+                    <div className={styles.img}></div>
+                    <div className={styles.img}></div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Product;
